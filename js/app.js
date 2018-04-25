c8c829697f802f8dfe269e3c6f379b1e22c9b221
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-04-25 15:27:14 -0400

    Reset enemy position when it goes off canvas

--- js/app.js
+++ js/app.js
@@ -6,12 +6,14 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
+
+    // Resets enemy position when it goes off canvas!
+    if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }        
 };
 
 // Draw the enemy on the screen, required method for game
