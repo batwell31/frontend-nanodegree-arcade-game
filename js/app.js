9dada6b2a4c8b76b873582396da0b355ffdfb7f6
Brandon Lothaire Atwell <batwell31@yahoo.com>
2018-04-25 15:42:53 -0400

    Add Player.prototype.render function to draw the player on the screen

--- js/app.js
+++ js/app.js
@@ -59,6 +59,12 @@ Player.prototype.update = function () {
         this.y = 380;
     }
 };
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
